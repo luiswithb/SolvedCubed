84bbed5b8c304a6ac5d8393adfaf586a6e6bf092
luiswithb <oracular.sky@gmail.com>
2022-03-10 13:35:38 -0600

    Added middle layer call, as well as updated the test function

--- main.py
+++ main.py
@@ -4,22 +4,33 @@ def test():
     for i in range(100):
         scramble()
         cross()
+        cubeM = deepcopy(cube)
         corners()
+        middleLayer()
 
-        if((not corners()) or (not cross())):
-            printCube()
+        if((cross()) and (corners()) and (middleLayer())): pass
+        else:
+            for i in cubeM:
+                print(i)
+            print('-'*60)
 
 def main():
-    #test()
+    # test() will only print if there is an error, the output without errors should only be
+    # the initial cube and the ending state
+    test() 
 
     scramble()
+    print("Scrambled cube:")
     printCube()
+
     cross()
     corners()
     middleLayer()
+
+    print("Ending cube:")
     printCube()
-    
-    print(solutionMoves)
+
+    #print(solutionMoves)
 
 if __name__ == "__main__":
     main()
