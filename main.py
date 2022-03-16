c963d1b30223ad606dd4e01717be5c46ef4ff63b
luiswithb <oracular.sky@gmail.com>
2022-03-16 16:55:13 -0500

    solver now orients the last layer

--- main.py
+++ main.py
@@ -13,19 +13,27 @@ def test():
             for i in cubeM:
                 print(i)
             print('-'*60)
+            
+        resetCube()
+
+def resetCube():
+    cube = deepcopy(solvedCube)
 
 def main():
     # test() will only print if there is an error, the output without errors should only be
     # the initial cube and the ending state
     test() 
 
-    scramble()
     print("Scrambled cube:")
+    s = scramble()
     printCube()
+    print("Scramble moves:",s)
 
     cross()
     corners()
     middleLayer()
+    while not orientLastLayer():
+        movesInBlueOrientation("F U R Ui Ri Fi") # algorithm to get a differnt orientation of the yellow layer
 
     print("Ending cube:")
     printCube()
