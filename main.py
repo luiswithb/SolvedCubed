24bd40b587ff8b2efc382e9020ba4b9360d00170
luiswithb <oracular.sky@gmail.com>
2022-04-04 11:08:47 -0500

    Added call to permuteLastLayer()

--- main.py
+++ main.py
@@ -19,11 +19,13 @@ def test():
 def resetCube():
     cube = deepcopy(solvedCube)
 
-def main():
-    # test() will only print if there is an error, the output without errors should only be
-    # the initial cube and the ending state
-    test() 
+def solution():
+    temp = []
+    for i in solutionMoves:
+        temp.append(i.__name__)
+    return temp
 
+def main():
     print("Scrambled cube:")
     s = scramble()
     printCube()
@@ -34,11 +36,38 @@ def main():
     middleLayer()
     while not orientLastLayer():
         movesInBlueOrientation("F U R Ui Ri Fi") # algorithm to get a differnt orientation of the yellow layer
+    p = permuteLastLayer()
+
+    # performs an algorithm that gets a different permutation in case the permutation is one of the ones not implemented yet
+    # runs until it finds one that has been implemented
+    while not p:
+        Ri()
+        U()
+        Ri()
+        D2()
+        R()
+        Ui()
+        Ri()
+        D2()
+        R2()
+        p = permuteLastLayer()
+
+    # last layer is solved but sometimes not oriented properly, this fixes that
+    if p:
+        while(cube != solvedCube):
+            movesInBlueOrientation("U")
 
     print("Ending cube:")
     printCube()
 
-    #print(solutionMoves)
+    if cube == solvedCube:
+        print("Solved!")
+        print("Solution moves:",solution())
+        print("Number of moves:", len(solution()))
+        return solution()
+    else:
+        print("Could not solve :(")
+        return False
 
 if __name__ == "__main__":
     main()
