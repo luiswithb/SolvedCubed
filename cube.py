cb6cef9813902a970d2ac9fe41eaf6901f9e5ec4
Poooooe <78933724+Poooooe@users.noreply.github.com>
2022-04-05 13:16:59 -0500

    Adding steps for prime move instead of a for loop

--- cube.py
+++ cube.py
@@ -254,23 +254,204 @@ def B2():
     for i in range(2):
         B()
 def Ri():
-    for i in range(3):
-        R()
+    previous_state = deepcopy(cube)
+
+    #move w to g
+    cube[6][5] = previous_state[3][5]
+    cube[7][5] = previous_state[4][5]
+    cube[8][5] = previous_state[5][5]
+
+    #move b to w
+    cube[3][5] = previous_state[0][5]
+    cube[4][5] = previous_state[1][5]
+    cube[5][5] = previous_state[2][5]
+
+    #move y to b
+    cube[0][5] = previous_state[5][9]
+    cube[1][5] = previous_state[4][9]
+    cube[2][5] = previous_state[3][9]
+
+    #move g to y
+    cube[5][9] = previous_state[6][5]
+    cube[4][9] = previous_state[7][5]
+    cube[3][9] = previous_state[8][5]
+
+    #move current face
+    cube[3][7] = previous_state[4][8]
+    cube[3][8] = previous_state[5][8]
+    cube[4][8] = previous_state[5][7]
+    cube[5][8] = previous_state[5][6]
+    cube[5][7] = previous_state[4][6]
+    cube[5][6] = previous_state[3][6]
+    cube[4][6] = previous_state[3][7]
+    cube[3][6] = previous_state[3][8]
+    solutionMoves.append(Ri)
 def Li():
-    for i in range(3):
-        L()
+    previous_state = deepcopy(cube)
+
+    #move w to b
+    cube[0][3] = previous_state[3][3]
+    cube[1][3] = previous_state[4][3]
+    cube[2][3] = previous_state[5][3]
+
+    #move b to y
+    cube[5][11] = previous_state[0][3]
+    cube[4][11] = previous_state[1][3]
+    cube[3][11] = previous_state[2][3]
+
+    #move y to g
+    cube[6][3] = previous_state[5][11]
+    cube[7][3] = previous_state[4][11]
+    cube[8][3] = previous_state[3][11]
+
+    #move g to w
+    cube[3][3] = previous_state[6][3]
+    cube[4][3] = previous_state[7][3]
+    cube[5][3] = previous_state[8][3]
+
+    #move current face
+    cube[3][1] = previous_state[4][2]
+    cube[3][2] = previous_state[5][2]
+    cube[4][2] = previous_state[5][1]
+    cube[5][2] = previous_state[5][0]
+    cube[5][1] = previous_state[4][0]
+    cube[5][0] = previous_state[3][0]
+    cube[4][0] = previous_state[3][1]
+    cube[3][0] = previous_state[3][2]
+    solutionMoves.append(Li)
 def Ui():
-    for i in range(3):
-        U()
+    previous_state = deepcopy(cube)
+
+    #move w to r
+    cube[3][6] = previous_state[3][3]
+    cube[3][7] = previous_state[3][4]
+    cube[3][8] = previous_state[3][5]
+
+    #move o to w
+    cube[3][3] = previous_state[3][0]
+    cube[3][4] = previous_state[3][1]
+    cube[3][5] = previous_state[3][2]
+
+    #move y to o
+    cube[3][0] = previous_state[3][9]
+    cube[3][1] = previous_state[3][10]
+    cube[3][2] = previous_state[3][11]
+
+    #move r to y
+    cube[3][9] = previous_state[3][6]
+    cube[3][10] = previous_state[3][7]
+    cube[3][11] = previous_state[3][8]
+
+    #move current face
+    cube[0][4] = previous_state[1][5]
+    cube[0][5] = previous_state[2][5]
+    cube[1][5] = previous_state[2][4]
+    cube[2][5] = previous_state[2][3]
+    cube[2][4] = previous_state[1][3]
+    cube[2][3] = previous_state[0][3]
+    cube[1][3] = previous_state[0][4]
+    cube[0][3] = previous_state[0][5]
+    solutionMoves.append(Ui)
 def Di():
-    for i in range(3):
-        D()
+    previous_state = deepcopy(cube)
+
+    #move w to o
+    cube[5][0] = previous_state[5][3]
+    cube[5][1] = previous_state[5][4]
+    cube[5][2] = previous_state[5][5]
+
+    #move r to w
+    cube[5][3] = previous_state[5][6]
+    cube[5][4] = previous_state[5][7]
+    cube[5][5] = previous_state[5][8]
+
+    #move y to r
+    cube[5][6] = previous_state[5][9]
+    cube[5][7] = previous_state[5][10]
+    cube[5][8] = previous_state[5][11]
+
+    #move o to y
+    cube[5][9] = previous_state[5][0]
+    cube[5][10] = previous_state[5][1]
+    cube[5][11] = previous_state[5][2]
+
+    #move current face
+    cube[6][4] = previous_state[7][5]
+    cube[6][5] = previous_state[8][5]
+    cube[7][5] = previous_state[8][4]
+    cube[8][5] = previous_state[8][3]
+    cube[8][4] = previous_state[7][3]
+    cube[8][3] = previous_state[6][3]
+    cube[7][3] = previous_state[6][4]
+    cube[6][3] = previous_state[6][5]
+    solutionMoves.append(Di)
 def Fi():
-    for i in range(3):
-        F()
+    previous_state = deepcopy(cube)
+
+    #move b to o
+    cube[5][2] = previous_state[2][3]
+    cube[4][2] = previous_state[2][4]
+    cube[3][2] = previous_state[2][5]
+
+    #move r to b
+    cube[2][3] = previous_state[3][6]
+    cube[2][4] = previous_state[4][6]
+    cube[2][5] = previous_state[5][6]
+
+    #move g to r
+    cube[3][6] = previous_state[6][5]
+    cube[4][6] = previous_state[6][4]
+    cube[5][6] = previous_state[6][3]
+
+    #move o to g
+    cube[6][5] = previous_state[5][2]
+    cube[6][4] = previous_state[4][2]
+    cube[6][3] = previous_state[3][2]
+
+    #move current face
+    cube[3][4] = previous_state[4][5]
+    cube[3][5] = previous_state[5][5]
+    cube[4][5] = previous_state[5][4]
+    cube[5][5] = previous_state[5][3]
+    cube[5][4] = previous_state[4][3]
+    cube[5][3] = previous_state[3][3]
+    cube[4][3] = previous_state[3][4]
+    cube[3][3] = previous_state[3][5]
+    solutionMoves.append(Fi)
 def Bi():
-    for i in range(3):
-        B()
+    previous_state = deepcopy(cube)
+
+    #move b to r
+    cube[3][8] = previous_state[0][3]
+    cube[4][8] = previous_state[0][4]
+    cube[5][8] = previous_state[0][5]
+
+    #move o to b
+    cube[0][3] = previous_state[5][0]
+    cube[0][4] = previous_state[4][0]
+    cube[0][5] = previous_state[3][0]
+
+    #move g to o
+    cube[5][0] = previous_state[8][5]
+    cube[4][0] = previous_state[8][4]
+    cube[3][0] = previous_state[8][3]
+
+    #move r to g
+    cube[8][5] = previous_state[3][8]
+    cube[8][4] = previous_state[4][8]
+    cube[8][3] = previous_state[5][8]
+
+    #move current face
+    cube[3][10] = previous_state[4][11]
+    cube[3][11] = previous_state[5][11]
+    cube[4][11] = previous_state[5][10]
+    cube[5][11] = previous_state[5][9]
+    cube[5][10] = previous_state[4][9]
+    cube[5][9] = previous_state[3][9]
+    cube[4][9] = previous_state[3][10]
+    cube[3][9] = previous_state[3][11]
+    solutionMoves.append(Bi)
+
 def R2i():
     for i in range(2):
         Ri()
